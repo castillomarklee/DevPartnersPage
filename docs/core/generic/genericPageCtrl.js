47277b4c20cc460936b43b93a838a228f0adb32b
Lee <castillomarklee03@gmail.com>
2020-05-05 14:08:39 +0800

    rewrite code to react

--- docs/core/generic/genericPageCtrl.js
+++
@@ -1,1 +0,0 @@
-"use strict";angular.module("app").controller("GenericPageController",["$scope","$sce","$uibModal","$stateParams","ProcessesService",function($scope,$sce,$uibModal,$stateParams,ProcessesService){$scope.img=$stateParams.pageDetails.headerImage,$scope.title=$stateParams.pageDetails.title,$scope.content=$stateParams.pageDetails.content}]);
---
+++ precache-manifest.54bc516f7dc30b255383d07a86f7e70a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3fe8e68e2566a3ed6e60eff6c73d52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6ccb94365a6ff740b08",
+    "url": "/static/css/main.b62f54d4.chunk.css"
+  },
+  {
+    "revision": "a50b060768290b6606f6",
+    "url": "/static/js/2.43a7ce77.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.43a7ce77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6ccb94365a6ff740b08",
+    "url": "/static/js/main.f4338780.chunk.js"
+  },
+  {
+    "revision": "90771c2cfbc3f8540204",
+    "url": "/static/js/runtime-main.1d895d97.js"
+  },
+  {
+    "revision": "6f8d003bcae669926e099aba5d574bc1",
+    "url": "/static/media/Alegreya-Bold.6f8d003b.otf"
+  },
+  {
+    "revision": "b40a55c28ff0b54a0be6364d7ded65ec",
+    "url": "/static/media/Alegreya-Italic.b40a55c2.otf"
+  },
+  {
+    "revision": "7135e7d9096f9fb121596b2b00804a49",
+    "url": "/static/media/AlegreyaSC-Regular.7135e7d9.otf"
+  },
+  {
+    "revision": "264ec931a3fef52df9c37c23b2dfe9a0",
+    "url": "/static/media/Images.264ec931.jpg"
+  },
+  {
+    "revision": "79260e5b693fac2ee373b659f01a2bdf",
+    "url": "/static/media/Inter-Bold.79260e5b.woff"
+  },
+  {
+    "revision": "7a8cc7241f766a142e15b2948804e547",
+    "url": "/static/media/Inter-Medium.7a8cc724.woff"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "/static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "beb497ba74d83866918761e250ab93b5",
+    "url": "/static/media/Office-bg@2x.beb497ba.jpg"
+  },
+  {
+    "revision": "a0c5a1e8a187cda36aeaf6cc75043bac",
+    "url": "/static/media/Office2@2x.a0c5a1e8.jpg"
+  },
+  {
+    "revision": "c884bb40f5cbc68f9ed5517f6151f478",
+    "url": "/static/media/banner-image1@2x.c884bb40.png"
+  },
+  {
+    "revision": "b9093d9ef8e8ea4669cd18d13bb58ea7",
+    "url": "/static/media/banner-image2@2x.b9093d9e.svg"
+  },
+  {
+    "revision": "c9f198a1b6befbdccfee8ff1b0285748",
+    "url": "/static/media/bg-2@2x.c9f198a1.jpg"
+  },
+  {
+    "revision": "c9c790a4e6e2ea307ac85a3a04ddd33e",
+    "url": "/static/media/devpartners-logo.c9c790a4.png"
+  },
+  {
+    "revision": "9d26e67a032920fba7122ec47ffe1b70",
+    "url": "/static/media/img-james@2x.9d26e67a.png"
+  }
+]);
